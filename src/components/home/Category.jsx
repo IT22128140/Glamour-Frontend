b51f481fcf6ec06a86123fa3cd9a8558fb20548e
IT22128140 <it22128140@my.sliit.lk>
2024-09-09 13:14:21 +0530

    Update API endpoints to use localhost:3000 instead of localhost:5555 added new fonts, logo, colors

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -14,11 +14,11 @@ const Category = (props) => {
       className="flex flex-col justify-around items-center w-[750px]"
     >
       <img src={props.image} className="w-full h-[600px] mt-[10%]" />
-      <h1 className="font-Lavish text-5xl m-2 text-primary ">
+      <h1 className="font-Aboreto text-5xl m-2 text-primary ">
         {props.heading}
       </h1>
       <Link to={props.link} state={props.state}>
-      <AnimButton classname={"bg-ternary w-fit text-bgc text-2xl font-Philosopher shadow-xl p-4 rounded-xl"} name={'Shop Now'}/>
+      <AnimButton classname={"bg-ternary w-fit text-primary text-2xl font-Philosopher shadow-xl p-4 rounded-xl"} name={'Shop Now'}/>
       </Link>
     </motion.div>
   );
